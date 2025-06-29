c8f2ebf2bea769e74156362acf792ca1ba66a017
HashigoHashi <ddaigotkh@gmail.com>
2025-06-29 21:25:32 +0900

    no_std

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 #![no_std]
-#![no_main]
 fn main() {
-    // println!("Hello, world!");
-    loop {}
-}
-
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+    println!("Hello, world!");
     loop {}
 }
