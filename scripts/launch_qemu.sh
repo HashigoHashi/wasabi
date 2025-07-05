d1481b2d5c0b72b797e1e27a1c5e80b49bc5d762
HashigoHashi <ddaigotkh@gmail.com>
2025-07-05 13:13:26 +0900

    sh修正

--- scripts/launch_qemu.sh
+++ scripts/launch_qemu.sh
@@ -7,6 +7,7 @@ rm -rf mnt
 mkdir -p mnt/EFI/BOOT/
 cp ${PATH_TO_EFI} mnt/EFI/BOOT/BOOTX64.EFI
 qemu-system-x86_64 \
+  -m 4G \
   -bios third_party/ovmf/RELEASEX64_OVMF.fd \
   -drive format=raw,file=fat:rw:mnt \
   -vnc :1
