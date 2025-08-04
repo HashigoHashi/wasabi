5a9d494d0029bd54e02d199d47f964d34399ea43
HashigoHashi <ddaigotkh@gmail.com>
2025-08-04 22:41:31 +0900

    fix

--- src/test_runner.rs
+++ src/test_runner.rs
@@ -3,7 +3,7 @@ use crate::qemu::QemuExitCode;
 use core::panic::PanicInfo;
 
 pub fn test_runner(_tests: &[&dyn FnOnce()]) -> ! {
-    exit_qemu(QemuExitCode::Success);
+    exit_qemu(QemuExitCode::Success)
 }
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
