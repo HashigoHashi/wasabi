7323f61651ed18821f54468c7f102f53dbfd7e9a
HashigoHashi <ddaigotkh@gmail.com>
2025-10-26 22:56:55 +0900

    test

--- src/uefi.rs
+++ src/uefi.rs
@@ -69,6 +69,9 @@ impl EfiMemoryDescriptor {
     pub fn number_of_pages(&self) -> u64 {
         self.number_of_pages
     }
+    pub fn physical_start(&self) -> u64 {
+        self.physical_start
+    }
 }
 
 // スタックオーバーフロー対策のため、メモリマップのサイズを64KB固定
