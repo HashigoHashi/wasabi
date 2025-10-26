eb4e718fa320e74163ab21b3132a30c3f5d9d7d7
HashigoHashi <ddaigotkh@gmail.com>
2025-10-27 00:19:01 +0900

    test

--- scripts/launch_qemu.sh
+++ scripts/launch_qemu.sh
@@ -7,11 +7,15 @@ PATH_TO_EFI="$1"
 rm -rf mnt
 mkdir -p mnt/EFI/BOOT/
 cp ${PATH_TO_EFI} mnt/EFI/BOOT/BOOTX64.EFI
+set +e
+mkdir -p log
 qemu-system-x86_64 \
   -m 4G \
   -bios third_party/ovmf/RELEASEX64_OVMF.fd \
   -drive format=raw,file=fat:rw:mnt \
-  -device isa-debug-exit,iobase=0xf4,iosize=0x01 #docker上出来どうする場合vncを使用するのでここを-vnc :1に変更する
+  -chardev stdio,id=char_com1,mux=on,logfile=log/com1.txt \
+  -serial chardev:char_com1 \
+  -device isa-debug-exit,iobase=0xf4,iosize=0x01 #docker上で起動する場合vncを使用するのでここを-vnc :1に変更する
 RETCODE=$?
 set -e
 if [ $RETCODE -eq 0 ]; then
